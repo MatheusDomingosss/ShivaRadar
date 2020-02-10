f858cc122514e9ce4974ffe082c5ef04740d1eab
MatheusDomingosss <55757631+MatheusDomingosss@users.noreply.github.com>
2020-02-10 15:09:34 -0300

    não funfa ufsc

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'exp://150.162.12.38:8242',
+  baseURL: 'http://150.162.208.47:8242',
 });
 
 export default api;
 
 // exp://150.162.12.38:19000
+
+// exp://4x-h5s.anonymous.mobile.exp.direct:80
+
